11b6b0ddc957798046f8da52fee041bb01f92ff2
Sumitrai1111 <sumitrai81125@gmail.com>
2025-08-24 13:13:15 +0530

    first commmit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://Sumitrai111:root1234@cluster0.niwl4yi.mongodb.net/food-delivery"
-    )
-    .then(() => {
-      console.log("DB Connected..!!");
-    });
+  await mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("DB Connected..!!");
+  });
 };
