02d5811667033e77059379204d67eb25022e1fac
Sumitrai1111 <sumitrai81125@gmail.com>
2025-08-24 13:32:42 +0530

    first commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDb = async () => {
-<<<<<<< HEAD
   await mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("DB Connected..!!");
   });
-=======
-  await mongoose
-    .connect(
-      process.env.MONGO_URL
-    )
-    .then(() => {
-      console.log("DB Connected..!!");
-    });
->>>>>>> b15e55de3b9cf7c8e8d04b0a686577bb313296c6
 };
