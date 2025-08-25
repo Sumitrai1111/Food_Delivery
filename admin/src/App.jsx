3018fa5f4ab5e4f63d27fac1fed2022000823e6a
Sumit Rai <110342639+Sumitrai1111@users.noreply.github.com>
2025-08-25 13:26:10 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "https://food-delivery-backend-nb8l.onrender.com";
+  const url = "https://food-delivery-bew5.onrender.com";
 
   return (
     <div>
